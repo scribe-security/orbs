4d40a40628018d84f2153540eff0272ca57af097
Sam Tholiya <tholiya.sam@outlook.in>
2022-07-24 18:53:05 +0200

    updated install script

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl -sSfL https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | sh 
+curl -sSfL https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | sh
+export PATH=$PATH:~/.scribe/bin
