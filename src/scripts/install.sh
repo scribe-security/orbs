cef785336d97f8962caca93b6a3373fef83185c5
Sam Tholiya <tholiya.sam@outlook.in>
2022-07-27 00:09:57 +0200

    install script updated

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+
+id=$(id -u)
+if [ "${id}" = 0 ]; then export SUDO=""; else export SUDO="sudo"; fi
+
+if dpkg --help >/dev/null ; then
+    export EXT=".deb"
+    $SUDO apt install curl jq -y
+elif apk list >/dev/null ; then
+    export EXT=".apk"
+    $SUDO apk add jq curl
+elif rpm --help >/dev/null ; then
+    export EXT=".rpm"
+    $SUDO yum install curl jq -y
+fi
+
 curl -sSfL https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | sh
 export PATH=$PATH:~/.scribe/bin
