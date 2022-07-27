f494315f2381a1d55201f8ac41a4f15dc7171757
Sam Tholiya <tholiya.sam@outlook.in>
2022-07-27 23:17:55 +0200

    added update to the script

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -5,12 +5,14 @@ if [ "${id}" = 0 ]; then export SUDO=""; else export SUDO="sudo"; fi
 
 if dpkg --help >/dev/null ; then
     export EXT=".deb"
+    $SUDO apt update
     $SUDO apt install curl jq -y
 elif apk list >/dev/null ; then
     export EXT=".apk"
     $SUDO apk add jq curl
 elif rpm --help >/dev/null ; then
     export EXT=".rpm"
+    $SUDO yum update
     $SUDO yum install curl jq -y
 fi
 
