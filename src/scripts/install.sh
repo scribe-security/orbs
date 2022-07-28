4ab4e592fcca4a1c35fe89a5d28b9f2204dc1f5e
houdini91 <mdstrauss91@gmail.com>
2022-07-28 09:29:16 +0300

    test mikey

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-id=$(id -u)
-if [ "${id}" = 0 ]; then export SUDO=""; else export SUDO="sudo"; fi
+# id=$(id -u)
+# if [ "${id}" = 0 ]; then export SUDO=""; else export SUDO="sudo"; fi
 
-if dpkg --help >/dev/null ; then
-    export EXT=".deb"
-    $SUDO apt update
-    $SUDO apt install curl jq -y
-elif apk list >/dev/null ; then
-    export EXT=".apk"
-    $SUDO apk add jq curl
-elif rpm --help >/dev/null ; then
-    export EXT=".rpm"
-    $SUDO yum update
-    $SUDO yum install curl jq -y
-fi
+# if dpkg --help >/dev/null ; then
+#     export EXT=".deb"
+#     $SUDO apt update
+#     $SUDO apt install curl jq -y
+# elif apk list >/dev/null ; then
+#     export EXT=".apk"
+#     $SUDO apk add jq curl
+# elif rpm --help >/dev/null ; then
+#     export EXT=".rpm"
+#     $SUDO yum update
+#     $SUDO yum install curl jq -y
+# fi
 
-curl -sSfL https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | sh -s -- -b /usr/local/bin
+# curl -sSfL https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | sh -s -- -b /usr/local/bin
 
 # 2DO use a more basic and common tool instead of curl.
-# wget -O /dev/stdout  https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | sh -s -- -b /usr/local/bin
+wget -O /dev/stdout  https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | sh -s -- -b /usr/local/bin
