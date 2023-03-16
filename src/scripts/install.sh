e1f4f6bf31cc5a3b87964bc04e4599c3fe64f99e
houdini91 <mdstrauss91@gmail.com>
2023-03-16 16:34:07 +0200

    typo

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -12,10 +12,10 @@ id=$(id -u)
 if [ "${id}" = 0 ]; then export SUDO=""; else export SUDO="sudo"; fi
 
 if is_command wget; then
-    wget -O /dev/stdout  https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | $SUDO sh -s -- -b /usr/local/bin
+    wget -O /dev/stdout  https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | $SUDO sh -s -- -b /usr/local/bin -d
     exit 0
 elif is_command curl; then
-    curl -sSfL https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | $SUDO sh -s -- -b /usr/local/bin
+    curl -sSfL https://raw.githubusercontent.com/scribe-security/misc/master/install.sh | $SUDO sh -s -- -b /usr/local/bin -d 
 else 
     if dpkg --help >/dev/null ; then
         export EXT=".deb"
